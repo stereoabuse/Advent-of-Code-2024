68afea8a403e0f65448d1dcdb3bac65b88c597c6
Chris Bowman <stereoabusestudios@gmail.com>
2024-12-07 17:36:10 -0500

    day 4 part 2

--- days/4.py
+++ days/4.py
@@ -1,12 +1,15 @@
+from collections import defaultdict
+
 with open('days/inputs/4') as f:
     input_text = f.read()
+
 
 grid = input_text.strip().split('\n')
 
 height, width = len(grid), len(grid[0])
 
-grid_dict = {complex(row, col): grid[row][col] for row in 
-             range(height) for col in range(width)}
+grid_dict = defaultdict(complex) | {complex(row, col): grid[row][col] for row in 
+                                    range(height) for col in range(width)}
 
 directions = [1, -1, 1j, -1j, 1+1j,-1-1j, 1-1j, -1+1j]
 
@@ -18,3 +21,23 @@ for pos in grid_dict:
             xmases += 1
 # part 1
 print(xmases)
+
+
+x_shape = {1+1j: 'M', -1-1j: 'S', 1-1j:'S', -1+1j:'M'}
+x_shape_reverse = {1+1j: 'S', -1-1j: 'M', 1-1j:'M', -1+1j:'S'}
+
+cross_count = 0
+grid_positions = list(grid_dict.keys())
+for position in grid_positions:
+    if grid_dict[position] == 'A':
+        diagonal1_matches = all(grid_dict.get(position + d) == x_shape[d] for d in [1+1j, -1-1j])
+        diagonal1_matches_flipped = all(grid_dict.get(position + d) == x_shape_reverse[d] for d in [1+1j, -1-1j])
+        
+        diagonal2_matches = all(grid_dict.get(position + d) == x_shape[d] for d in [1-1j, -1+1j])
+        diagonal2_matches_flipped = all(grid_dict.get(position + d) == x_shape_reverse[d] for d in [1-1j, -1+1j])
+        
+        if (diagonal1_matches or diagonal1_matches_flipped) and (diagonal2_matches or diagonal2_matches_flipped):
+            cross_count += 1
+
+# part 2
+print(cross_count)
