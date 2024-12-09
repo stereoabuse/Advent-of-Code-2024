ac53cf448bd868908e333d650817ffa86ce2e35f
Chris Bowman <stereoabusestudios@gmail.com>
2024-12-08 23:13:48 -0500

    day 6 part 1

--- days/6.py
+++ days/6.py
@@ -1,16 +1,5 @@
-input_text = '''....#.....
-.........#
-..........
-..#.......
-.......#..
-..........
-.#..^.....
-........#.
-#.........
-......#...'''
-
-# with open('days/inputs/6') as f:
-#     input_text = f.read()
+with open('days/inputs/6') as f:
+    input_text = f.read()
 
 G = {(r, c): char for r, line in enumerate(input_text.splitlines()) for c, char in enumerate(line)}
 
@@ -18,7 +7,7 @@ current = [key for key, value in G.items() if value == '^'][0]
 directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
 direction_index = 0
 visited = set()
-
+  
 while current in G:
     next_current = tuple(v1 + v2 for v1, v2 in zip(current, directions[direction_index % 4]))
     if next_current in G:
@@ -30,12 +19,9 @@ while current in G:
         else:
             current = next_current
             visited.add(current)
-
-
     else:
         break
 
 
 # part 1
 print(len(visited))
-
