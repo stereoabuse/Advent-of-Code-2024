b030552978c39f653261ab285e43a514718ae7d5
stereoabuse <stereoabusestudios@gmail.com>
2024-12-15 17:38:20 -0500

    day 14 p1, p2

--- days/14.py
+++ days/14.py
@@ -12,14 +12,12 @@ for second in count(0):
     seen = set()
     q = [0] * 4
     for line in input_data.splitlines():
-        # q = [0] * 4
         px, py, vx, vy = map(int, re.findall(r'-?\d+', line))
         px_1 = (px + (vx * second)) % WIDTH
         py_1 = (py + (vy * second)) % HEIGHT
         seen.add((px_1, py_1))
         if px_1 == WIDTH // 2 or py_1 == HEIGHT // 2:
             continue
-
         if px_1 < WIDTH // 2 and py_1 < HEIGHT // 2:
             q[0] += 1  
         elif px_1 >= WIDTH // 2 and py_1 < HEIGHT // 2:
@@ -28,7 +26,6 @@ for second in count(0):
             q[2] += 1  
         elif px_1 >= WIDTH // 2 and py_1 >= HEIGHT // 2:
             q[3] += 1  
-
     if second == 100:
         print(prod(q)) # part 1
     if len(seen) == len(input_data.splitlines()):
