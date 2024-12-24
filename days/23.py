6d12bc2bd92ecd9f94b597831bfdf27cb965fb1b
stereoabuse <stereoabusestudios@gmail.com>
2024-12-23 23:26:34 -0800

    day 23 part 2

--- days/23.py
+++ days/23.py
@@ -1,17 +1,19 @@
 from collections import defaultdict
 from itertools import combinations
+import networkx as nx
 
 with open('days/inputs/23') as f:
     input_text = f.read()
 
 
 G = defaultdict(list)
+GRAPH = nx.Graph()
 
 for line in input_text.splitlines():
     node1, node2 = line.split('-')
+    GRAPH.add_edge(*line.split('-'))
     G[node1].append(node2)
     G[node2].append(node1)
-
 
 connections = set()
 
@@ -25,5 +27,9 @@ for item in connections:
     if any(computer.startswith('t') for computer in item):
         ans += 1
 
-
 print(ans)
+
+# gotta use networkx
+connected_comps = list(nx.find_cliques(GRAPH))
+party = max(connected_comps, key=len)
+print(','.join(sorted(party)))
