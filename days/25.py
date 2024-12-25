7e5e08f158f2e94fd6eaf661e571e1fcf098b2b2
stereoabuse <stereoabusestudios@gmail.com>
2024-12-24 22:46:16 -0800

    day 25 part 1 simplified

--- days/25.py
+++ days/25.py
@@ -1,27 +1,17 @@
-with open('days/inputs/25') as f:
-    schematics = f.read().split('\n\n')
-
-locks, keys = [], []
+from itertools import combinations
 
 def gridify(schematic):
-    return {(r,c): char for r, row in enumerate(schematic) 
-                        for c, char in enumerate(row)}
+    return {(r,c): char for r, row in enumerate(schematic) for c, char in enumerate(row)}
 
-for schematic in schematics:
-    schematic = schematic.splitlines()
-    if all(char == '#' for char in schematic[0]):
-        locks.append(gridify(schematic))
-    else:
-        keys.append(gridify(schematic))
+with open('days/inputs/25') as f:
+    schematics = [gridify(schematic) for schematic in f.read().split('\n\n')]
 
 total = 0
-for key in keys:
-    for lock in locks:
-        works = True
-        for key_char, lock_char in zip(key.values(), lock.values()):
-            if key_char == '#' and lock_char == '#':
-                works = False
-                break
-        if works:
-            total += 1
+for (a, b) in combinations(schematics,2):
+    works = True
+    for x, y in zip(a.values(), b.values()):
+        if x == y == '#':
+            works = False
+    if works:
+        total += 1
 print(total)
