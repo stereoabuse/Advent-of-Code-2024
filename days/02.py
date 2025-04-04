bda328c701b530dde5dd37e90fc1f903d410340d
stereoabuse <stereoabusestudios@gmail.com>
2025-04-04 09:21:25 -0700

    Add type hints to day 2 implementation

--- days/02.py
+++ days/02.py
@@ -1,21 +1,22 @@
 from itertools import pairwise
+from typing import List, Tuple
 
 with open('days/inputs/2') as f:
-    reports = [[int(x) for x in report.strip().split()] for report in f.readlines()]
+    reports: List[List[int]] = [[int(x) for x in report.strip().split()] for report in f.readlines()]
 
-def is_safe_report(line):
-    monotonic = (line == sorted(line) or line == sorted(line, reverse=True))
-    return monotonic and all(1 <= abs(a - b) <= 3 for (a,b) in pairwise(line))
+def is_safe_report(line: List[int]) -> bool:
+    monotonic: bool = (line == sorted(line) or line == sorted(line, reverse=True))
+    return monotonic and all(1 <= abs(a - b) <= 3 for (a, b) in pairwise(line))
 
 # part 1
 print(sum(1 for line in reports if is_safe_report(line)))
 
 # part 2
-safe_with_dampener = 0
+safe_with_dampener: int = 0
 for full_report in reports:
-    safety = False
+    safety: bool = False
     for i in range(len(full_report)): 
-        dampened = full_report[:i] + full_report[i+1:]
+        dampened: List[int] = full_report[:i] + full_report[i+1:]
         if is_safe_report(dampened):
             safety = True
     if safety:
